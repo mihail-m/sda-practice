87848f8295bb7e4b02232e4b03cb79db0c10a43f
Mihail Mihaylov <mihailm@vmware.com>
2020-11-22 23:18:00 +0200

    Fix indentation.

--- week 07/code/e.cpp
+++ week 07/code/e.cpp
@@ -2,67 +2,67 @@
 using namespace std;
 
 void solve() {
-	int n, k;
-	cin >> n >> k;
+    int n, k;
+    cin >> n >> k;
 
-	set<pair<int, int>> power;
-	set<pair<int, int>> index;
-	for (int i = 0; i < n; i++) {
-		int num;
-		cin >> num;
+    set<pair<int, int>> power;
+    set<pair<int, int>> index;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
 
-		power.insert({num, i});
-		index.insert({i, num});
-	}
+        power.insert({num, i});
+        index.insert({i, num});
+    }
 
-	int team = 0;
-	vector<int> ans(n);
+    int team = 0;
+    vector<int> ans(n);
 
-	while (power.empty() == false) {
-		auto player = *(--power.end());
-		power.erase(player);
+    while (power.empty() == false) {
+        auto player = *(--power.end());
+        power.erase(player);
 
-		auto it = index.find({player.second, player.first});
-		
-		vector<set<pair<int, int>>::iterator> to_remove;
+        auto it = index.find({player.second, player.first});
+        
+        vector<set<pair<int, int>>::iterator> to_remove;
 
-		it++;
-		for (int i = 1; i <= k && it != index.end(); i++) {
-			power.erase({(*it).second, (*it).first});
-			to_remove.push_back(it);
-			ans[(*it).first] = team + 1;
-			it++;
-		}
+        it++;
+        for (int i = 1; i <= k && it != index.end(); i++) {
+            power.erase({(*it).second, (*it).first});
+            to_remove.push_back(it);
+            ans[(*it).first] = team + 1;
+            it++;
+        }
 
-		it = index.find({player.second, player.first});
-		for (int i = 0; i <= k; i++) {
-			power.erase({(*it).second, (*it).first});
-			to_remove.push_back(it);
-			ans[(*it).first] = team + 1;
-			
-			if (it == index.begin()) {
-				break;
-			}
-			it--;
-		}
+        it = index.find({player.second, player.first});
+        for (int i = 0; i <= k; i++) {
+            power.erase({(*it).second, (*it).first});
+            to_remove.push_back(it);
+            ans[(*it).first] = team + 1;
+            
+            if (it == index.begin()) {
+                break;
+            }
+            it--;
+        }
 
-		for (auto iter : to_remove) {
-			index.erase(iter);
-		}
+        for (auto iter : to_remove) {
+            index.erase(iter);
+        }
 
-		team = (team + 1) % 2;
-	}
+        team = (team + 1) % 2;
+    }
 
-	for (int i = 0; i < n; i++) {
-		cout << ans[i];
-	}
-	cout << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << ans[i];
+    }
+    cout << "\n";
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	solve();
-	return 0;
+    solve();
+    return 0;
 }
